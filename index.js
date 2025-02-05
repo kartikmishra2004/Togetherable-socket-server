c1cc9b9ac8901b08f7d8055a28503687147ff721
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-05 23:19:22 +0530

    CORS update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", 'https://togetherable.vercel.app'],
         methods: ["GET", "POST"]
     }
 });
